f268692c1a60b28d16a84705b3aa25d2eb99d2b1
kabir <jxwazxzhp@126.com>
2018-03-28 01:08:44 +0800

    add ByteTest

---
+++ day01/ByteTest.java
@@ -0,0 +1,12 @@
+/**
+ * ByteTest
+ */
+public class ByteTest {
+
+    public static void main(String[] args) {
+        byte a = 'a';
+        char b = 'a';
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
